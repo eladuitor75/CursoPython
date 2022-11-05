f71b83180b2818c89f0196be3bd130541f1b6d4b
deivis.ramirez <deivisr25@gmail.com>
2022-11-05 18:15:18 -0500

    carga archivo

---
+++ CursoPython/condicionales.py
@@ -0,0 +1,9 @@
+numero1 = 15
+numero2 = 10
+
+if numero1 > numero2:
+    print ('El numero1 es mas grande')
+elif numero1 == numero2:
+    print ('Los números son iguales')
+else:
+    print ('El numero2 es mas grande')
