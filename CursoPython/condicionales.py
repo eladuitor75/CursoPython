1162f5d2edb2df099715c434a1bdbca96152932d
deivis.ramirez <deivisr25@gmail.com>
2022-11-05 18:45:21 -0500

    cuarto commit

--- CursoPython/condicionales.py
+++ CursoPython/condicionales.py
@@ -2,7 +2,7 @@ numero1 = 15
 numero2 = 10
 
 if numero1 > numero2:
-    print ('El numero1 es mas grande')
+    print ('El numero1 es mas Grande')
 elif numero1 == numero2:
     print ('Los números son Iguales')
 else:
